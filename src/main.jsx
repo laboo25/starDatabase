528cad789be2af7f8d8581d90f9fcadbd4de9717
lab00 <89362256+laboo25@users.noreply.github.com>
2024-06-12 22:00:08 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -20,75 +20,81 @@ import Search from "./components/search/Search";
 import Images from "./components/images/Images";
 import GetAlbum from "./components/album/GetAlbum";
 
-Fancybox.bind('[data-fancybox="gallery"]', {
+// Function to check if screen width is less than or equal to 375px
+const isMobileScreen = () => window.innerWidth <= 375;
+
+// Fancybox options
+const fancyboxOptions = {
   Carousel: {
-        transition: "slide",
-      },
+    transition: "slide",
+  },
   Toolbar: {
     display: {
       left: ["infobar"],
-      middle: [
-        "zoomIn",
-        "zoomOut",
-        "rotateCW",
-        "flipX",
-        "flipY",
-      ],
-      right: ["slideshow", "fullscreen", "thumbs", "download", "close"],
+      middle: isMobileScreen()
+        ? [] // Empty array for middle buttons if on mobile screen
+        : ["zoomIn", "zoomOut", "rotateCW", "flipX", "flipY"],
+      right: isMobileScreen()
+        ? ["thumbs", "download", "close"] // Show only these on mobile
+        : ["slideshow", "fullscreen", "thumbs", "download", "close"],
     },
   },
-});
+};
+
+// Bind Fancybox with options
+Fancybox.bind('[data-fancybox="gallery"]', fancyboxOptions);
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
   },
   {
     path: "/star/:_id",
-    element: <StarDeltails/>,
+    element: <StarDeltails />,
   },
   {
     path: "/query",
-    element: <Search/>,
+    element: <Search />,
   },
   {
     path: "/albums",
-    element: <GetAlbum/>,
+    element: <GetAlbum />,
   },
   {
     path: "/images",
-    element: <Images/>,
+    element: <Images />,
   },
   {
     path: "/create",
-    element: <Create/>,
-    children: [{
-      path: "/create",
-      element: <CreateStar/>,
-    },
-    {
-      path: '/create/create-bio',
-      element: <CreateStarBio/>,
-    },
-    {
-      path: '/create/create-images',
-      element: <CreateImages/>,
-    },
-    {
-      path: '/create/create-album',
-      element: <CreateAlbums/>,
-    },
-    {
-      path: '/create/update-all',
-      element: <UpdateAll/>,
-    },
-    {
-      path: '/create/update-star',
-      element: <StarUpdate/>,
-    },
-  ]
-  }
+    element: <Create />,
+    children: [
+      {
+        path: "/create",
+        element: <CreateStar />,
+      },
+      {
+        path: "/create/create-bio",
+        element: <CreateStarBio />,
+      },
+      {
+        path: "/create/create-images",
+        element: <CreateImages />,
+      },
+      {
+        path: "/create/create-album",
+        element: <CreateAlbums />,
+      },
+      {
+        path: "/create/update-all",
+        element: <UpdateAll />,
+      },
+      {
+        path: "/create/update-star",
+        element: <StarUpdate />,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
