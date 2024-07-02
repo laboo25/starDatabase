332e246a5f9adabfdfba3b77b7acb609e321172f
lab00 <89362256+laboo25@users.noreply.github.com>
2024-07-03 00:08:39 +0600

    Add files via upload

--- src/components/album/album/GetAlbum.jsx
+++ src/components/album/album/GetAlbum.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import ModalAlbum from './ModalAlbum';
-import './getAlbum.css'
+import './getAlbum.css';
 
 const GetAlbum = () => {
   const [albums, setAlbums] = useState([]);
   const [sortedAlbums, setSortedAlbums] = useState([]);
   const [selectedAlbum, setSelectedAlbum] = useState(null);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [isSorted, setIsSorted] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(localStorage.getItem('modalVisible') === 'true');
+  const [sortField, setSortField] = useState('date'); // 'date' or 'name'
+  const [sortOrder, setSortOrder] = useState('asc'); // 'asc' or 'desc'
 
   useEffect(() => {
     fetch('https://stardb-api.onrender.com/api/stars/albums/get-all-albums')
@@ -19,38 +20,79 @@ const GetAlbum = () => {
       .catch(error => console.error('Error fetching albums:', error));
   }, []);
 
+  useEffect(() => {
+    const handlePopState = (event) => {
+      if (isModalVisible) {
+        closeModal();
+        event.preventDefault();
+      }
+    };
+
+    window.addEventListener('popstate', handlePopState);
+
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, [isModalVisible]);
+
+  useEffect(() => {
+    sortAlbums();
+  }, [sortField, sortOrder, albums]);
+
   const openModal = (album) => {
     setSelectedAlbum(album);
     setIsModalVisible(true);
+    document.body.style.overflow = 'hidden';
+    window.history.pushState({ modalOpen: true }, '');
   };
 
   const closeModal = () => {
     setIsModalVisible(false);
     setSelectedAlbum(null);
+    document.body.style.overflow = 'auto';
+    window.history.back();
   };
 
-  const toggleSorting = () => {
-    if (isSorted) {
-      setSortedAlbums(albums);
-    } else {
-      const sorted = [...albums].sort((a, b) => a.albumname.localeCompare(b.albumname));
-      setSortedAlbums(sorted);
-    }
-    setIsSorted(!isSorted);
+  const sortAlbums = () => {
+    const sorted = [...albums].sort((a, b) => {
+      if (sortField === 'name') {
+        return sortOrder === 'asc'
+          ? a.albumname.localeCompare(b.albumname)
+          : b.albumname.localeCompare(a.albumname);
+      } else {
+        return sortOrder === 'asc'
+          ? new Date(a.createdAt) - new Date(b.createdAt)
+          : new Date(b.createdAt) - new Date(a.createdAt);
+      }
+    });
+    setSortedAlbums(sorted);
+  };
+
+  const toggleSortField = () => {
+    setSortField(prev => (prev === 'date' ? 'name' : 'date'));
+  };
+
+  const toggleSortOrder = () => {
+    setSortOrder(prev => (prev === 'asc' ? 'desc' : 'asc'));
   };
 
   return (
-    <div className='px-10'>
+    <div id='getAlbum' className=''>
       <div className='text-[#dfdfdf] text-[10px]'>{`${sortedAlbums.length} albums available`}</div>
-      <button onClick={toggleSorting} className='mb-4 px-4 py-2 bg-blue-500 text-white rounded'>
-        {isSorted ? 'Date ⇅' : 'Name ⇅'}
-      </button>
+      <div className='mb-4'>
+        <button onClick={toggleSortField} className='px-4 py-2 bg-blue-500 text-white rounded'>
+          {sortField === 'date' ? 'name' : 'date'}
+        </button>
+        <button onClick={toggleSortOrder} className='ml-2 px-4 py-2 bg-blue-500 text-white rounded'>
+          {sortOrder === 'asc' ? 'a↑' : 'z↓'}
+        </button>
+      </div>
       {sortedAlbums.length === 0 ? (
         <p>Loading...</p>
       ) : (
-        <div className='w-full flex gap-3 py-10 flex-wrap'>
+        <div id='wrapper' className='w-full flex py-10 flex-wrap'>
           {sortedAlbums.map(album => (
-            <div key={album._id} className='album-container w-1/4'>
+            <div key={album._id} className='album-container'>
               {album.albumimages.length > 0 && (
                 <div className='w-full aspect-video' onClick={() => openModal(album)}>
                   <img 
@@ -72,7 +114,10 @@ const GetAlbum = () => {
           albumname={selectedAlbum.albumname} 
           length={selectedAlbum.albumimages.length} 
           images={selectedAlbum.albumimages} 
-          onClose={closeModal} 
+          onClose={closeModal}
+          albumId={selectedAlbum._id}  // Pass albumId here
+          sortField={sortField}
+          sortOrder={sortOrder}
         />
       )}
     </div>
--- src/components/album/album/ModalAlbum.jsx
+++ src/components/album/album/ModalAlbum.jsx
@@ -1,19 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import './modalAlbum.css'
+import React, { useEffect, useState } from 'react';
+import './modalAlbum.css';
 import axios from 'axios';
+import { Select, Input, Modal, Button } from 'antd'; // Import Select and Input components from Ant Design
+import category from '../../category.json';
+import { HiDotsVertical } from "react-icons/hi";
 
-const ModalAlbum = ({ visible, albumname, length, images, onClose }) => {
+const { Option } = Select;
+
+const ModalAlbum = ({ visible, albumname, length, images, onClose, albumId, sortField, sortOrder }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [starnames, setStarnames] = useState([]);
   const [starnameFilters, setStarnameFilters] = useState({});
   const [tagFilters, setTagFilters] = useState({});
+  const [tags, setTags] = useState([]);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(null);
+  const [editTagsModalVisible, setEditTagsModalVisible] = useState(false);
+  const [newTags, setNewTags] = useState([]);
+  const [currentTags, setCurrentTags] = useState([]);
+  const [editAlbumModalVisible, setEditAlbumModalVisible] = useState(false); // State for edit album modal
+  const [editAlbumname, setEditAlbumname] = useState(albumname); // State to store edited album name
+  const [editStarname, setEditStarname] = useState(images[selectedImageIndex]?.starname || []); // State to store edited star names
 
   useEffect(() => {
-    const fetchStarnames = async () => {
+    const fetchStarnamesAndTags = async () => {
       try {
         const response = await axios.get('https://stardb-api.onrender.com/api/stars/create-star/get-all-star');
-        const fetchedStarnames = response.data.map(star => star.starname);
+        const fetchedStarnames = response.data.map(star => star.starname).sort();
         setStarnames(fetchedStarnames);
+
+        const fetchedTags = images.reduce((acc, image) => {
+          image.tags.forEach(tag => {
+            if (!acc.includes(tag)) {
+              acc.push(tag);
+            }
+          });
+          return acc;
+        }, []).sort();
+        setTags(fetchedTags);
 
         const initialStarnameFilters = fetchedStarnames.reduce((acc, starname) => {
           acc[starname] = false;
@@ -21,12 +44,12 @@ const ModalAlbum = ({ visible, albumname, length, images, onClose }) => {
         }, {});
         setStarnameFilters(initialStarnameFilters);
       } catch (error) {
-        console.error('Error fetching starnames:', error);
+        console.error('Error fetching starnames and tags:', error);
       }
     };
 
-    fetchStarnames();
-  }, []);
+    fetchStarnamesAndTags();
+  }, [images]);
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
@@ -40,6 +63,67 @@ const ModalAlbum = ({ visible, albumname, length, images, onClose }) => {
     setTagFilters(prev => ({ ...prev, [tag]: !prev[tag] }));
   };
 
+  const toggleImageOptions = (index) => {
+    setSelectedImageIndex(index === selectedImageIndex ? null : index);
+  };
+
+  const openEditTagsModal = (tags) => {
+    setCurrentTags(tags);
+    setNewTags(tags);
+    setEditTagsModalVisible(true);
+  };
+
+  const saveTags = async () => {
+    if (!albumId || selectedImageIndex === null) {
+      console.error('Album ID or selected image index is undefined');
+      return;
+    }
+    try {
+      const updatedImages = [...images];
+      updatedImages[selectedImageIndex].tags = newTags.sort();
+
+      await axios.put(`https://stardb-api.onrender.com/api/stars/albums/update/${albumId}`, {
+        albumname,
+        starname: updatedImages[selectedImageIndex].starname,
+        albumimages: updatedImages
+      });
+
+      setEditTagsModalVisible(false);
+      setTags([...new Set(newTags)].sort());
+    } catch (error) {
+      console.error('Error saving tags:', error);
+    }
+  };
+
+  const handleEditAlbumname = () => {
+    setEditAlbumModalVisible(true);
+  };
+
+  const handleEditAlbum = async () => {
+    try {
+      // Perform API call to update albumname and starname[] here
+      console.log('Updated albumname:', editAlbumname);
+      console.log('Updated starname:', editStarname);
+
+      // Close modal after updating
+      setEditAlbumModalVisible(false);
+    } catch (error) {
+      console.error('Error updating album:', error);
+    }
+  };
+
+  const handleCancelEditAlbum = () => {
+    setEditAlbumModalVisible(false);
+  };
+
+  const handleAlbumnameChange = (e) => {
+    setEditAlbumname(e.target.value);
+  };
+
+  const handleStarnameSelectChange = (value) => {
+    setEditStarname(value);
+  };
+
   const filteredImages = images.filter(image => {
     const matchesStarname = Object.keys(starnameFilters).every(starname => !starnameFilters[starname] || image.starname === starname);
     const matchesTags = Object.keys(tagFilters).every(tag => !tagFilters[tag] || image.tags.includes(tag));
@@ -51,51 +135,105 @@ const ModalAlbum = ({ visible, albumname, length, images, onClose }) => {
   return (
     <div className='modal-main'>
       <div className={`modal-sidebar ${sidebarOpen ? 'open' : 'closed'}`}>
-        <div>
-          <p>Filter by Starname</p>
-          <div>
-            {starnames.map(starname => (
-              <label key={starname}>
-                <input
-                  type="checkbox"
-                  checked={starnameFilters[starname] || false}
-                  onChange={() => handleStarnameChange(starname)}
-                />
-                {starname}
-              </label>
-            ))}
-          </div>
-        </div>
-        <div>
-          <p>Filter by Tags</p>
-          <div>
-            {['tag1', 'tag2', 'tag3'].map(tag => (
-              <label key={tag}>
-                <input type="checkbox" checked={tagFilters[tag] || false} onChange={() => handleTagChange(tag)} />
-                {tag}
-              </label>
-            ))}
+        <div id='filter-option'>
+          <div id=''>
+            <p>Filter by Tags</p>
+            <div id='tags-wrapper'>
+              {tags.map(tag => (
+                <div key={tag}>
+                  <input type="checkbox" checked={tagFilters[tag] || false} onChange={() => handleTagChange(tag)} id={`tag-${tag}`} />
+                  <label htmlFor={`tag-${tag}`}>{tag} [{tag.length}]</label>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
       <div className="custom-modal-overlay">
         <div>
-          <button onClick={toggleSidebar} className='top-2 left-2'>Open Sidebar</button>
+          <button onClick={toggleSidebar} className='top-2 left-2 pl-6 text-[30px]'>◧</button>
         </div>
         <button className="custom-modal-close" onClick={onClose}>×</button>
         <div className="custom-modal-content">
           <div className='pb-10 text-[20px]'>
             <h3 draggable={false}>{albumname} <span className='text-red-500'>{`x${length}`}</span></h3>
+            <span><button onClick={handleEditAlbumname}>edit</button></span>
           </div>
           <div className="image-grid card">
             {filteredImages.map((image, index) => (
-              <a href={image.imageurl} data-fancybox="gallery" key={index}>
-                <img src={image.thumburl} alt={`Album image ${index + 1}`} draggable={false} />
-              </a>
+              <div key={index} className='relative'>
+                <a href={image.imageurl} data-fancybox="gallery">
+                  <img src={image.thumburl} alt={`Album image ${index + 1}`} draggable={false} />
+                </a>
+                <div className='w-full min-h-[30px] pt-2'>
+                  <ul className='flex gap-2'>
+                    {image.tags.sort().map((tag, idx) => (
+                      <li className='hover:text-[blue] cursor-default text-black' key={idx}>{tag}</li>
+                    ))}
+                  </ul>
+                </div>
+                <div className='edit-modal absolute bottom-0 right-0'>
+                  <button className='edit-btn' onClick={() => toggleImageOptions(index)}><HiDotsVertical /></button>
+                  {selectedImageIndex === index && (
+                    <div className="options-menu">
+                      <button className='edit-tags' onClick={() => openEditTagsModal(image.tags)}>Edit</button>
+                      <button>Delete</button>
+                    </div>
+                  )}
+                </div>
+              </div>
             ))}
           </div>
         </div>
       </div>
+
+      {editTagsModalVisible && (
+        <div className="edit-tags-modal">
+          <h2>Edit Tags</h2>
+          <div className="modal-content">
+            <Select
+              mode="multiple"
+              value={newTags}
+              onChange={setNewTags}
+              style={{ width: '100%' }}
+              placeholder="Select tags"
+              className='w-full'
+            >
+              {Array.isArray(category.tags) && category.tags.sort().map((tag, index) => (
+                <Option key={index} value={tag}>
+                  {tag}
+                </Option>
+              ))}
+            </Select>
+            <div className='buttons'>
+              <Button type="primary" className='bg-blue-500' onClick={saveTags}>Save</Button>
+              <Button type="primary" className='bg-blue-500' onClick={() => setEditTagsModalVisible(false)}>Cancel</Button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <Modal
+        title="Edit Album"
+        open={editAlbumModalVisible}
+        onOk={handleEditAlbum}
+        onCancel={handleCancelEditAlbum}
+      >
+        <Input value={editAlbumname} onChange={handleAlbumnameChange} />
+        <Select
+          mode="multiple"
+          value={editStarname}
+          onChange={handleStarnameSelectChange}
+          style={{ width: '100%' }}
+          placeholder="Select star names"
+        >
+          {starnames.map((starname, index) => (
+            <Option key={index} value={starname}>
+              {starname}
+            </Option>
+          ))}
+        </Select>
+      </Modal>
     </div>
   );
 };
